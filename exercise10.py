dbcdc841755c5b7437ac779bf08e3f21d9fb01f3
Piotr Grzybowski <57330536+GribaHub@users.noreply.github.com>
2020-02-20 22:09:09 +0100

    Update exercise10.py

--- exercise10.py
+++ exercise10.py
@@ -46,5 +46,10 @@ print(common)
 print("Solution without duplicates: ", end='')
 print(list(set(common)))
 
+# List is a collection which is ordered and changeable. Allows duplicate members.
+# Tuple is a collection which is ordered and unchangeable. Allows duplicate members.
+# Set is a collection which is unordered and unindexed. No duplicate members.
+# Dictionary is a collection which is unordered, changeable and indexed. No duplicate members.
+
 # Test of conditions
 # print((1 in b) and (1 not in common))
